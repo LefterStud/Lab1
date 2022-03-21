19b8c2085c21f9340dac3d7568d827b0e5187192
LefterStud <101552997+LefterStud@users.noreply.github.com>
2022-03-21 11:34:42 +0200

    Task 42 is completed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,21 @@ package com.company;
  * Lefter Olexii, #18
  */
 public class Main {
+    /**
+     * Вычислить у = cos x + cos x^2 + cos x^3 + ... + cos x^n.
+     * */
+    private static void task42(){
+    int NumDegree;
+    double y=0;
+    double x=45;
+    int n=6;
 
+    for(NumDegree = 1; NumDegree <=n; NumDegree++){
+        y += Math.pow(Math.cos(x),NumDegree);
+    }
+        System.out.println(y);
+    }
     public static void main(String[] args) {
-
+    task42();
     }
 }
