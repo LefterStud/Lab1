42db2431bd5d8628a0959154393d607b84a5268f
LefterStud <101552997+LefterStud@users.noreply.github.com>
2022-03-22 10:40:36 +0200

    Task 67&92 is completed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,13 +12,41 @@ public class Main {
     double y=0;
     double x=45;
     int n=6;
-
     for(NumDegree = 1; NumDegree <=n; NumDegree++){
         y += Math.pow(Math.cos(x),NumDegree);
     }
         System.out.println(y);
     }
+
+    private static void task67(){
+        int time;
+        int NumAnim=1;
+        for(time=3; time<=24; time+=3){
+            NumAnim*=2;
+            System.out.println("Через "+time+" часов будет "+NumAnim+" клеток.");
+        }
+    }
+
+    private static void task92(){
+        int[] Mark=new int[19];
+        double x;
+        float average;
+        float sum = 0;
+        for (int i=0; i<=19; i++){
+            x=(int) (Math.random()*12)+1;
+            sum+=x;
+            System.out.println(x);
+        }
+        average = sum/ Mark.length;
+        System.out.println("Sum is "+sum);
+        System.out.println("Average of Marks is " + average);
+    }
+
+
+
     public static void main(String[] args) {
-    task42();
+    //  task42();
+    //  task67();
+        task92();
     }
 }
