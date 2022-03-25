62bab16c7c88408f2b680403b821b98220aac18d
LefterStud <101552997+LefterStud@users.noreply.github.com>
2022-03-25 20:59:45 +0200

    task 142 is completed and task117 is started

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -18,6 +18,10 @@ public class Main {
         System.out.println(y);
     }
 
+    /**
+     * Одноклеточное животное амёба каждые 3 часа делится на 2 клетки. Определить,
+     * сколько клеток будет через 3, 6, 9,..., 24 часа, если первоначально была одна амёба.
+     * */
     private static void task67(){
         int time;
         int NumAnim=1;
@@ -27,26 +31,77 @@ public class Main {
         }
     }
 
+    /**
+     *Известны оценки по физике каждого из 20 учеников класса.
+     * Определить среднюю оценку по классу.
+     * */
     private static void task92(){
-        int[] Mark=new int[19];
         double x;
         float average;
         float sum = 0;
         for (int i=0; i<=19; i++){
             x=(int) (Math.random()*12)+1;
             sum+=x;
-            System.out.println(x);
         }
-        average = sum/ Mark.length;
+        average = sum/20;
         System.out.println("Sum is "+sum);
         System.out.println("Average of Marks is " + average);
     }
 
+    /**
+     *Составить программу, которая печатает таблицу умножения
+     * и сложения натуральных чисел в шестнадцатеричной системе счисления.
+     * */
+    private static void task117(){
+        int col;
+        int row;
+        int RowCol = 17;
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+    /**
+     *Выведите на экран все четырёхразрядные числа, в записи которых нет одинаковых цифр.
+     * */
+    private static void task142(){
+        int u;
+        int d;
+        int h;
+        int t;
+        for(int i=999; i<=9999; i++){
+            u=i%10;
+            d=(i%100)/10;
+            h=(i%1000)/100;
+            t=(i%10000)/1000;
+            if((u!=d)&&(u!=h)&&(u!=t)&&(d!=h)&&(d!=t)&&(h!=t)){
+                System.out.println(i);
+            }
+        }
+
+    }
 
     public static void main(String[] args) {
     //  task42();
     //  task67();
-        task92();
+    //  task92();
+      task117();
+    //  task142();
     }
 }
